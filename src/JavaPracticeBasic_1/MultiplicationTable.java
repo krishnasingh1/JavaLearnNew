9dee43a71f9eda5bf63022206cda2da9bbeca060
Krishana Pal <krishnasingh.ks62@gmail.com>
2022-12-14 23:25:17 +0530

    Multiplication Table

--- src/JavaPracticeBasic_1/MultiplicationTable.java
+++ src/JavaPracticeBasic_1/MultiplicationTable.java
@@ -1,5 +1,17 @@
 package JavaPracticeBasic_1;
 
-public class MultiplicationTable {
+import java.util.Scanner;
 
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Table Number : ");
+        int num = sc.nextInt();
+        System.out.println("The Multiplication Table is : ");
+
+        for (int i = 1; i <= 10; i++) {
+            int mult = num * i;
+            System.out.println(num + " x " + i + " = " + mult);
+        }
+    }
 }
